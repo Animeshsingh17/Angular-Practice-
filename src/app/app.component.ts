8179bfe0b6cd6c859b43ecc82ea09c2c80053b91
Animeshsingh17 <animesh.singh170199@gmail.com>
2022-12-05 19:15:41 +0530

    First status 14/66

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myOne';
+  displayVal='';
+  number=100;
+  getName(name:string){
+    console.log(name)
+  }
+  getData(box:string){
+   console.log("This  keyup function is here ",box)
+  }
+  getData1(tox:any){
+    console.log("This  focus function is here ",tox)
+  }
+  getNameP(box1:string){
+    console.warn(box1)
+    this.displayVal=box1
+  }
+  counter(type:string){
+   type==='add' ? this.number++ : this.number--
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserAuthModule } from './user-auth/user-auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    UserAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
