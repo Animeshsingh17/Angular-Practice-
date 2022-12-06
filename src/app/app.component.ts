15ff8da1a39d29867c9553b7ddeaa456213beae2
Animeshsingh17 <animesh.singh170199@gmail.com>
2022-12-06 18:18:48 +0530

    done 28/66  PART I

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,94 @@
 import { Component } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  color = 'red';
+  color1: string = 'green';
+  users = ['Animesh', 'Peter', 'dube', 'akash', 'Aditya'];
+  userDetails = [
+    {
+      name: 'Animesh',
+      id: 1,
+      email: 'animesh.singh170199@gmail.com',
+      phone: +91 - 9521463259,
+      socialaccount: ['fb', 'insta', 'twitter', 'linkdin'],
+    },
+    {
+      name: 'Peter',
+      id: 2,
+      email: 'anpeter99@gmail.com',
+      phone: +9259,
+      socialaccount: ['fb', 'insta', 'twitter'],
+    },
+    {
+      name: 'dube',
+      id: 3,
+      email: 'andube199@gmail.com',
+      phone: +913259,
+      socialaccount: ['fb', 'insta'],
+    },
+    {
+      name: 'akash',
+      id: 4,
+      email: 'aniaka.si99@gmail.com',
+      phone: +9159,
+      socialaccount: ['twitter', 'linkdin'],
+    },
+  ];
+  display = false;
   title = 'myOne';
-  displayVal='';
-  number=100;
-  getName(name:string){
-    console.log(name)
+  displayVal = '';
+  number = 100;
+  disable = true;
+  show = true;
+  userData: any = {};
+  getName(name: string) {
+    console.log(name);
   }
-  getData(box:string){
-   console.log("This  keyup function is here ",box)
+  getData(box: string) {
+    console.log('This  keyup function is here ', box);
   }
-  getData1(tox:any){
-    console.log("This  focus function is here ",tox)
+  getData1(tox: any) {
+    console.log('This  focus function is here ', tox);
   }
-  getNameP(box1:string){
-    console.warn(box1)
-    this.displayVal=box1
+  getNameP(box1: string) {
+    console.warn(box1);
+    this.displayVal = box1;
   }
-  counter(type:string){
-   type==='add' ? this.number++ : this.number--
+  counter(type: string) {
+    type === 'add' ? this.number++ : this.number--;
+  }
+  updateColor(color1: string) {
+    this.color1 = 'blue';
+  }
+  getFormData(data: NgForm) {
+    console.log(data);
+    this.userData = data;
+    console.log(this.userData, 'hello data');
+  }
+  toggle() {
+    this.display = !this.display;
+  }
+  page = 4;
+  hidden = false;
+
+  toggleBadgeVisibility() {
+    this.hidden = !this.hidden;
+  }
+  list:any[]=[]
+  addTask(item:string){
+    this.list.push({id:this.list.length,name:item})
+    console.warn(this.list)
+  }
+  removeTask(id:number){
+   this.list=this.list.filter(item=>item.id!==id)
+  }
+  data=10;
+  updateChild(){
+    this.data=Math.floor(Math.random()*10)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,33 @@ import { UserAuthModule } from './user-auth/user-auth.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap'
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MatSliderModule} from "@angular/material/slider"
+import {MatButtonModule} from '@angular/material/button'
+import {MatBadgeModule} from '@angular/material/badge';
+import { ChildComponent } from './child/child.component';
+import { UserDetailComponent } from './user-detail/user-detail.component'
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HeaderComponent,
+    ChildComponent,
+    UserDetailComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserAuthModule
+    UserAuthModule,
+    FormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatButtonModule,
+    MatBadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
